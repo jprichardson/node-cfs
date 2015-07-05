0d54392e457bff8ced4935e0718e7cdb42280706
JP Richardson <jprichardson@gmail.com>
2015-07-05 13:41:51 -0500

    index: LRU caching

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var fs = require('fs')
+var LRU = require('lru-cache')
 var os = require('os')
 var path = require('path')
 var WriteStream = fs.WriteStream
 
 function createWriteStream (fn, options) {
-  var tmpFile = Math.random().toString().slice(2) + '.tmp'
-  var defaultPath = fn() || path.join(os.tmpdir(), tmpFile)
+  var defaultPath = fn() || tempFile()
   var ws = new WriteStream(defaultPath, options)
+
+  function cacheDispose (filePath, fd) {
+    fs.close(fd, function (err) {
+      if (err) ws.emit('error', err)
+    })
+  }
+
+  var cache = LRU({
+    max: 100,
+    dispose: cacheDispose,
+    maxAge: 1000 * 60 * 60
+  })
+
+  ws.on('close', function () {
+    cache.reset()
+  })
+
+  ws.on('finish', function () {
+    cache.reset()
+  })
 
   var oldWrite = ws._write
   ws._write = function (data, encoding, done) {
@@ -15,16 +35,18 @@ function createWriteStream (fn, options) {
     // no sense closing and opening if it's the same
     if (newPath === ws.path) {
       return oldWrite.call(ws, data, encoding, done)
-    }
-
-    if (ws.fd == null) {
-      openNewPath()
     } else {
-      fs.close(ws.fd, openNewPath)
+      openNewPathAndWrite()
     }
 
-    function openNewPath () {
+    function openNewPathAndWrite () {
       ws.path = newPath
+      var fd = cache.get(newPath)
+      if (fd) {
+        ws.fd = fd
+        return oldWrite.call(ws, data, encoding, done)
+      }
+
       fs.open(ws.path, ws.flags, ws.mode, function (err, fd) {
         if (err) {
           ws.destroy()
@@ -33,12 +55,18 @@ function createWriteStream (fn, options) {
         }
 
         ws.fd = fd
+        cache.set(newPath, fd)
         oldWrite.call(ws, data, encoding, done)
       })
     }
   }
 
   return ws
+}
+
+function tempFile () {
+  var tmp = Math.random().toString().slice(2) + '.tmp'
+  return path.join(os.tmpdir(), tmp)
 }
 
 module.exports = {
