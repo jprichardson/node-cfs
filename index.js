048ed5c1cce98ca06fd07d9946b8cb827e78597f
JP Richardson <jprichardson@gmail.com>
2015-11-09 13:15:35 -0600

    index: completely refactored => instead of one fs.writeStream and multiple fds, use multiple fs.writeStreams

--- index.js
+++ index.js
@@ -1,98 +1,68 @@
 var assign = require('object-assign')
 var fs = require('fs')
-var LRU = require('lru-cache')
 var mkdirp = require('mkdirp')
-var os = require('os')
 var path = require('path')
-var WriteStream = fs.WriteStream
+var LRU = require('lru-cache')
+var Writable = require('stream').Writable
 
-function createWriteStream (fn, options) {
-  var defaultPath = fn() || tempFile()
-  var ws = new WriteStream(defaultPath, options)
-
-  function cacheDispose (filePath, fd) {
-    fs.close(fd, function (err) {
-      if (err) ws.emit('error', err)
-    })
-  }
-
+function createWriteStream (pathResolver, options) {
   var cacheOptions = assign({
     max: 100,
     dispose: cacheDispose,
     maxAge: 1000 * 60 * 60
   }, options.cacheOptions)
 
-  var cache = LRU(cacheOptions)
+  var cfs = new Writable()
+  cfs._writesPending = 0
+  cfs._cache = LRU(cacheOptions)
 
-  ws.on('close', function () {
-    cache.reset()
-  })
-
-  ws.on('finish', function () {
-    cache.reset()
-  })
-
-  var oldWrite = ws._write
-  ws._write = function (data, encoding, done) {
-    var newPath = fn(data, encoding)
-
-    // no sense closing and opening if it's the same
-    if (newPath === ws.path) {
-      return oldWrite.call(ws, data, encoding, done)
+  cfs._write = function (data, encoding, done) {
+    cfs._writesPending += 1
+    var newPath = pathResolver(data, encoding)
+    var ws = this._cache.get(newPath)
+    if (ws) {
+      ws.write(data, encoding, function () { cfs._writesPending -= 1 })
+      done()
     } else {
-      openNewPathAndWrite()
-    }
-
-    function openNewPathAndWrite () {
-      ws.path = newPath
-      var fd = cache.get(newPath)
-      if (fd) {
-        ws.fd = fd
-        return oldWrite.call(ws, data, encoding, done)
-      }
-
-      // hacky solution for Node v4
-      try {
-        var dir = path.dirname(ws.path)
-        mkdirp.sync(dir)
-        fd = fs.openSync(ws.path, ws.flags, ws.mode)
-      } catch (err) {
-        ws.destroy()
-        ws.emit('error', err)
-        return
-      }
-
-      ws.fd = fd
-      cache.set(newPath, fd)
-      oldWrite.call(ws, data, encoding, done)
+      var dir = path.dirname(newPath)
+      mkdirp(dir, function (err) {
+        if (err) return cfs.emit('error', err)
+        var newws = fs.createWriteStream(newPath, options)
+        newws.on('error', function (err) { cfs.emit('error', err) })
+        cfs._cache.set(newPath, newws)
+        newws.write(data, encoding, function () { cfs._writesPending -= 1 })
+        done()
+      })
     }
   }
 
   // won't see performance boost in Node v4
-  ws._writev = function (chunks, done) {
-    var len = chunks.length
+  cfs._writev = null
 
-    function callback () {
-      len -= 1
-      if (len === 0) done()
-    }
+  var oldEnd = cfs.end.bind(cfs)
+  cfs.end = function (data, encoding, callback) {
+    if (data) cfs._write(data, encoding)
 
-    chunks.forEach(function (c) {
-      setImmediate(function () {
-        ws._write(c.chunk, c.encoding, callback)
-      })
+    var pendingCheck = setInterval(function () {
+      // console.log(cfs._writesPending)
+      if (cfs._writesPending > 0) return
+      clearInterval(pendingCheck)
+      cfs._cache.reset()
+      callback && callback()
+    }, 50)
+  }
+
+  // maybe put something here...
+  cfs.once('finish', function () { })
+
+  function cacheDispose (filePath, ws) {
+    var cache = this
+    ws.end(function () {
+      if (cache.itemCount === 0) oldEnd() // trigger 'finish' event
     })
   }
 
-  // expose fd cache
-  ws._cache = cache
-
-  return ws
-}
-
-function tempFile () {
-  var tmp = Math.random().toString().slice(2) + '.tmp'
-  return path.join(os.tmpdir(), tmp)
+  return cfs
 }
 
 module.exports = {
